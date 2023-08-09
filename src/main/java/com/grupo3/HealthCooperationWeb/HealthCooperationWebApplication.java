848aae91441af88af173e566bf11456c934c87bb
FedeBirge <fedeikky@gmail.com>
2023-08-09 11:15:03 -0300

    first commit

---
+++ src/main/java/com/grupo3/HealthCooperationWeb/HealthCooperationWebApplication.java
@@ -0,0 +1,13 @@
+package com.grupo3.HealthCooperationWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCooperationWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCooperationWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo3/HealthCooperationWeb/HealthCooperationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo3.HealthCooperationWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCooperationWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
