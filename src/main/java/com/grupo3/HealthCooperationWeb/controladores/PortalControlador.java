fac926782e7d10de72c1793bc91fc11d6a756b54
FedeBirge <fedeikky@gmail.com>
2023-08-10 10:23:08 -0300

    Contorlador para levantar la vista del index2

--- src/main/java/com/grupo3/HealthCooperationWeb/controladores/PortalControlador.java
+++ src/main/java/com/grupo3/HealthCooperationWeb/controladores/PortalControlador.java
@@ -17,6 +17,10 @@ public class PortalControlador {
         @GetMapping("/")
     public String index(ModelMap modelo) {
         
+        
+        
+        
+        
         return "index.html";
     }
 }
