b26de3c924b27979e09d63d1d43101ed0b8e4477
FedeBirge <fedeikky@gmail.com>
2023-08-10 09:54:42 -0300

    Contorlador para levantar la vista del index

---
+++ src/main/java/com/grupo3/HealthCooperationWeb/controladores/PortalControlador.java
@@ -0,0 +1,22 @@
+package com.grupo3.HealthCooperationWeb.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author feder
+ */
+
+@Controller
+@RequestMapping("/")
+public class PortalControlador {
+    
+        @GetMapping("/")
+    public String index(ModelMap modelo) {
+        
+        return "index.html";
+    }
+}
