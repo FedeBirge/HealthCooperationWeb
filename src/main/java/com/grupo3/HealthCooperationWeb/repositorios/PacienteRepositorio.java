5371b9ffc69beb0176ec2e49ba60d11f8ac3d31e
Mariela Cordoba <marielaancordoba@gmail.com>
2023-08-14 12:47:54 -0300

    Terminado Paciente, entidad, servicio y controlador

--- src/main/java/com/grupo3/HealthCooperationWeb/repositorios/PacienteRepositorio.java
+++ src/main/java/com/grupo3/HealthCooperationWeb/repositorios/PacienteRepositorio.java
@@ -2,6 +2,7 @@
 package com.grupo3.HealthCooperationWeb.repositorios;
 
 import com.grupo3.HealthCooperationWeb.entidades.Paciente;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,8 @@ import org.springframework.stereotype.Repository;
 //creando interface marcada con @Repository que extiende de JpaRepository
 //manejando la entidad Paciente cuya llave es de tipo de dato string
 public interface PacienteRepositorio extends JpaRepository<Paciente, String>{
+
+    public Optional<Paciente> findById(Paciente usuario);
     
    // MSJ LT: la queryno es necesaria, se usa lo del padre
     
