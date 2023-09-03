9525076d35f537ac0978eb2d8043172bfa62a71b
FedeBirge <fedeikky@gmail.com>
2023-09-03 13:51:59 -0300

    envio de mensajes desde index y contacto

--- src/main/java/com/grupo3/HealthCooperationWeb/controladores/EmailControlador.java
+++ src/main/java/com/grupo3/HealthCooperationWeb/controladores/EmailControlador.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/email")
@@ -26,16 +27,30 @@ public class EmailControlador {
 
      @PostMapping("/contacto")
     public String enviarCorreo(ModelMap modelo,@RequestParam String nombre, @RequestParam String apellido, @RequestParam String email,
-            @RequestParam String telefono, @RequestParam String esp, @RequestParam String msj) {
+            @RequestParam String telefono, @RequestParam String esp, @RequestParam String msj, RedirectAttributes redirectAttributes) {
         
         try{            
-        emailServ.sendEmail(nombre, apellido, email, telefono, esp, msj);
-        //modelo.put("exito", "!Informacion enviada con exito!");
-        return "contacto.html"; // Redirecciona a la página principal después de enviar el correo
+        emailServ.sendEmail(nombre, apellido, email, telefono, esp, "Soy Profesional: "+msj);
+        redirectAttributes.addFlashAttribute("exito", "!Informacion enviada con exito!");
+        return "redirect:/";// Redirecciona a la página principal después de enviar el correo
 
         }catch (MyException ex){
-           // modelo.put("error", ex.getMessage());
-        return "contacto.html"; // Redirecciona a la página principal después de enviar el correo
+            redirectAttributes.addFlashAttribute("error", ex.getMessage());
+        return "redirect:/"; // Redirecciona a la página principal después de enviar el correo
+    }
+    }
+     @PostMapping("/publico")
+    public String enviarCorreo(ModelMap modelo,@RequestParam String nombre, @RequestParam String email,
+            @RequestParam String telefono, @RequestParam String msj, RedirectAttributes redirectAttributes) {
+        
+        try{            
+        emailServ.sendEmail(nombre, "no", email, telefono, "no","SOY usuario: "+ msj);
+        redirectAttributes.addFlashAttribute("exito", "!Informacion enviada con exito!");
+        return "redirect:/"; // Redirecciona a la página principal después de enviar el correo
+
+        }catch (MyException ex){
+            redirectAttributes.addFlashAttribute("error", ex.getMessage());
+        return "redirect:/"; // Redirecciona a la página principal después de enviar el correo
     }
     }
 }
